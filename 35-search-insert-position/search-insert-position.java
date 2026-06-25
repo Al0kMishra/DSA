7bebd952467a02893418b6325f80ee5737dc5763
Alok Mishra <rogalokmishra2110@gmail.com>
2026-06-25 10:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (81.89%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,17 +1,38 @@
+// class Solution {
+//     public int searchInsert(int[] nums, int target) {
+//         int idx =0;
+//         for (int i = 0; i<nums.length; i++){
+//             if(nums[i]>=target){
+//                 idx = i;
+//                 break; 
+//             }
+//             else{
+//                 if(nums[nums.length -1]<target){
+//                     return nums.length;
+//                 }
+//             }
+//         }
+//         return idx;
+//     }
+// }
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int idx =0;
-        for (int i = 0; i<nums.length; i++){
-            if(nums[i]>=target){
-                idx = i;
-                break; 
+     public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        
+        while(l<=h){
+            int mid = (l + h)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+               l= mid+1;
             }
             else{
-                if(nums[nums.length -1]<target){
-                    return nums.length;
-                }
+               h = mid-1;
+
             }
         }
-        return idx;
+        return l;
     }
 }
