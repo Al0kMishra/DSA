b9ea2491187cdb5127ca82686a15c68c17d52fdd
Alok Mishra <rogalokmishra2110@gmail.com>
2026-06-26 10:18:28 +0530

    Time: 14 ms (48.74%) | Memory: 41.9 MB (71.44%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -6,7 +6,7 @@ public class Solution extends VersionControl {
         int left = 0;
         int right = n;
         while (left<right){
-            int mid = left +( right-left) / 2;
+            int mid = left +(right-left) / 2;
             if(isBadVersion(mid)){
                 right = mid;    
             } 
